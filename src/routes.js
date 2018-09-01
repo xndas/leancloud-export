8a9a027c09287f2f955bd6c13665eae36a3b49ea
Xiyao <xiyaolee@outlook.com>
2018-09-01 22:34:56 +0800

    ss

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import Connect from './components/Connect'
-import Main from './components/Main'
-
-const routes = [
-  { path: '/', component: Main },
-  { path: '/connect', component: Connect }
-]
-
-export default routes
